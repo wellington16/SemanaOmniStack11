56c461ac5e1e0199d9cb4392fc5dd69d30eff521
Wellington Luiz Antonio <wellingtonluiz123456@gmail.com>
2020-03-30 11:38:51 -0300

    Funções avançadas, Teste unitários e integração.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
+
+
 const OngControler = require('./controllers/OngController')
 const IncidentsControler = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
@@ -8,14 +11,37 @@ const routes = express.Router()
 
 // recursos da ONG
 routes.get('/ongs',OngControler.index)
-routes.post('/ongs', OngControler.create)
+
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp:Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}),OngControler.create)
 
 // recursos dos Incidents
 routes.post('/incidents', IncidentsControler.create)
-routes.get('/incidents', IncidentsControler.index)
-routes.delete('/incidents/:id', IncidentsControler.delete)
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}) , IncidentsControler.index)
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}),IncidentsControler.delete)
+
 // recursos do Profile
-routes.get('/profile', ProfileController.index)
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index)
 
 // recurso da Session
 routes.post('/sessions', SessionController.create) 
